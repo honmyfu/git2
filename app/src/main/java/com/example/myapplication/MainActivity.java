3c83bb2b88cc2bba65a85a6a2748a63949b96b24
honmyfu <1296688743@qq.com>
2022-09-29 17:05:23 +0800

    初始化

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void m1(View view) {
+        Toast.makeText(this,"click",Toast.LENGTH_SHORT).show();
+    }
+}
