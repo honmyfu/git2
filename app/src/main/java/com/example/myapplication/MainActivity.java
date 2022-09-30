3048331ecd9670cb469ab29f4802de10de756a59
honmyfu <1296688743@qq.com>
2022-09-30 08:02:07 +0800

    第一次修改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void m1(View view) {
-        Toast.makeText(this,"click3",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"click4",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"click2",Toast.LENGTH_SHORT).show();
     }
 }
